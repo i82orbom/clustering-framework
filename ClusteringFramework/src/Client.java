b3ad79a5b0c42da7ea3f63831849a68dbcb3ae75
mariottf2@gmail.com <mariottf2@gmail.com@1f5ef8f9-85b3-dad6-0273-f961f9018ca6>
2013-05-13 17:41:06 +0000

    Object serializing over network

--- ClusteringFramework/src/Client.java
+++ ClusteringFramework/src/Client.java
@@ -1,25 +1,48 @@
-import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 
 public class Client {
 
-	public static void main (String...args) throws UnknownHostException, IOException{
-		 String sentence;
-		  String modifiedSentence;
-		  BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-		  Socket clientSocket = new Socket("localhost", 4567);
-		  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		  sentence = inFromUser.readLine();
-		  outToServer.writeBytes(sentence + '\n');
-		  modifiedSentence = inFromServer.readLine();
-		  System.out.println("FROM SERVER: " + modifiedSentence);
-		  clientSocket.close();
+	private Socket clientSocket;
+	private OutputStream outToServer;
+	private InputStream inFromServer;
+	
+	public Client(String addr, int port) throws UnknownHostException, IOException{
+		this.clientSocket = new Socket(addr, port);
+		this.outToServer= this.clientSocket.getOutputStream(); 
+		this.inFromServer = this.clientSocket.getInputStream();
+	}
+	
+	public void sendToServer(MyRandomClass bos) throws IOException{
+		ObjectOutputStream os = new ObjectOutputStream(this.outToServer);
+		os.writeObject(bos);
+		os.flush();
+		os.close();
+	}
+	
+	public void receiveFromServer(){
 		
 	}
+	
+//	public static void main (String...args) throws UnknownHostException, IOException{
+//		 String sentence;
+//		  String modifiedSentence;
+//		  BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
+//		  Socket clientSocket = new Socket("169.254.169.23", 1879);
+//		  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+//		  BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//		  sentence = inFromUser.readLine();
+//		  outToServer.writeBytes(sentence + '\n');
+//		  modifiedSentence = inFromServer.readLine();
+//		  System.out.println("FROM SERVER: " + modifiedSentence);
+//		  clientSocket.close();
+//		
+//	}
 }
--- ClusteringFramework/src/TestSerializable.java
+++ ClusteringFramework/src/TestSerializable.java
@@ -1,3 +1,9 @@
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.UnknownHostException;
+
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+
 
 public class TestSerializable {
 
@@ -5,8 +11,29 @@ public class TestSerializable {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Client cl = null;
+		try {
+			cl = new Client("localhost", 4567);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		MyRandomClass obj = new MyRandomClass("Meh", "Moh");
 
+
+		try {
+			cl.sendToServer(obj);
+			System.out.println("Object serialized...");
+			System.out.println("Object sent!");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
