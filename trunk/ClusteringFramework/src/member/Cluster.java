5350ce4ffb58ba17bf7ac8bc7b77166e36424e8b
mariottf2@gmail.com <mariottf2@gmail.com@1f5ef8f9-85b3-dad6-0273-f961f9018ca6>
2013-05-14 09:59:07 +0000

    MEH

--- trunk/ClusteringFramework/src/member/Cluster.java
+++ trunk/ClusteringFramework/src/member/Cluster.java
@@ -1,13 +1,108 @@
 package member;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Cluster {
 	
-	private Set<MemberInstance> members;
-	private int clusterID;
+	private Set<MemberInfo> members;
+	private double clusterID;
 	
+	
+
+	public double getClusterID() {
+		return clusterID;
+	}
+
 	public Cluster(){
+		members = null;
+	}
+	
+	public Set<MemberInfo> getMembers() {
+		return members;
+	}
+
+	public void setMembers(Set<MemberInfo> members) {
+		this.members = members;
+	}
+	
+	public void joinCluster(){
+		// Try to get one cluster member info
+		Iterator<MemberInfo> it = members.iterator();
+		MemberInfo extractedMember = null;
+		boolean memberConnected = false;
+		Socket sk = null;
+		while (it.hasNext()){ /** Cluster has members (maybe not connected) */
+			extractedMember = it.next();
+			
+			/** Try connection to member */
+			try {
+				sk = new Socket(extractedMember.getAddress(), extractedMember.getPort());
+				memberConnected = true;
+			} catch (Exception e){
+				/** Connection not possible */
+				memberConnected = false;
+			}
 		
+		}
+		
+		if (memberConnected == true){
+			  try {
+				DataOutputStream outToMember = new DataOutputStream(sk.getOutputStream());
+				outToMember.writeBytes("JOIN");
+				DataInputStream inFromMember = new DataInputStream(sk.getInputStream());
+				/** Wait for response **/
+				String memberResponse = inFromMember.readLine();
+				if (memberResponse.equalsIgnoreCase("OK")){
+					memberResponse = inFromMember.readLine();
+					/** Member response should be the cluster id */
+					this.clusterID = Double.parseDouble(memberResponse);
+					/** Now should answer with the IDs of the rest of the cluster */
+					boolean done = false;
+					
+					while (!done){
+						try{
+							ObjectInputStream oi = new ObjectInputStream(sk.getInputStream());
+							MemberInfo receivedObject = (MemberInfo)oi.readObject();
+							oi.close();
+
+							Iterator<MemberInfo> it2 = this.members.iterator();
+							while (it2.hasNext()){
+								MemberInfo mem = it2.next();
+								if (mem.getAddress() == receivedObject.getAddress() && mem.getPort() == receivedObject.getPort()){
+									mem.setMemberID(mem.getMemberID());
+								}
+							}
+						}
+						catch (IOException e){
+							done = true;
+						}
+					}
+					
+					outToMember.writeBytes("ACK");
+					
+				}
+			} catch (Exception e) {
+				System.err.println("Error getting output stream to member.");
+				e.printStackTrace();
+			}
+
+		}
+		else{
+			// Create new cluster
+			createNewCluster();
+		}
+		
+	}
+	
+	private void createNewCluster(){
+		// At this point none of the Members inside MemberInfo should be online
+		this.clusterID = Math.random();
+		System.out.println("New cluster [" + this.clusterID + "] created.");
 	}
 }
---
+++ trunk/ClusteringFramework/src/member/MemberInfo.java
@@ -0,0 +1,34 @@
+package member;
+
+public class MemberInfo {
+
+	private double memberID;
+	private int port;
+	private String address;
+	
+	
+	public MemberInfo(String address, int port){
+		this.address = address;
+		this.port = port;
+	}
+	
+	public double getMemberID() {
+		return memberID;
+	}
+	public void setMemberID(double memberID) {
+		this.memberID = memberID;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
